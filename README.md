f6bb05aba77acae2d9c60eb8ed3c15fdade4bf81
norman <85535885@qq.com>
2023-02-17 20:20:10 +0800

    .

--- README.md
+++ README.md
@@ -1,16 +1,21 @@
-# reverpod_test
+# Flutter Riverpod 状态管理器使用测试
 
-A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## 总结:
+1. `flutter pub add flutter_riverpod` 即可安装
+2. main.dart中 引用 `'package:flutter_riverpod/flutter_riverpod.dart';`
+3. main函数改为`void main() {
+   runApp(const ProviderScope(child: MyApp()));
+   }`
+4. 需要使用到多组件共享状态全局状态等的地方 也入main.dart中一样引用.
+5. 使用`final counterProvider = StateProvider((ref) => 0);`定义一个状态为直接单个状态的provider
+6. `class StateController extends ChangeNotifier {
+   int _count = 0;
+   int get count => _count;
+   set count(int value) {
+   _count = value;
+   notifyListeners();
+   }
+   }`可以定义一个状态为多个状态的控制器,然后使用`final counterProvider = StateNotifierProvider((ref) => StateController());`定义一个状态为多个状态的provider来使用
+7. 使用ConsumerStatefulWidget可以管理自身的状态而不需要借助Riverpod把状态升级到外面
+8. 通过使用Riverpod可以在不同的Widget中共享状态,StateLess的组件也可以变成像有状态一样.因为他的状态在外面
+9. 我们不可直接赋值触发状态变更吗?哦,或许那样使用起来更简单,但是代码可读性差一点,有时候不知道是页面刷新操作.所以我们使用`context.read(counterProvider).state = 0;`来触发状态变更
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(ProviderScope(child: MyApp()));
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -16,27 +16,49 @@ class MyApp extends StatelessWidget {
       ),
       home:
         Column(
-          children: [
+          children: const [
             RView(title: "RiverPodTest"),
             RController(),
+            RStatelessView(),
+            RStatelessController(),
           ],
         )
     );
   }
 }
 
-class StateController extends StateNotifier<ViewState> {
-  StateController() : super(ViewState()
-      ..count = 10
-  );
-  void increment() => state = ViewState()..count = state.count + 1;
+//region 这种通过notifyListeners手动控制是否通知监听者的方式可以直接使用ViewState里面的一个getter,setter来更新UI,但是写起来很麻烦.而且和Flutter本身自带的Provider没有语法区别.其他的区别也暂时没有发现.
+class StateController extends ChangeNotifier {
+  int _count = 0;
+  int get count => _count;
+  set count(int value) {
+    _count = value;
+    notifyListeners();
+  }
 }
+//endregion
 
 var stateControllerProvider =
-StateNotifierProvider<StateController, ViewState>((ref) => StateController());
+ChangeNotifierProvider<StateController>((ref) => StateController());
+
+
+//region 这种方式直接使用ref.read(stateControllerProvider).count++;不能更新UI
+// final stateControllerProvider = StateProvider<ViewState>((ref) => ViewState());
+//endregion
+
+//region 这种方式直接使用ref.read(stateControllerProvider).count++; 不能更新UI
+// final stateControllerProvider = StateNotifierProvider<StateController, ViewState>((ref) => StateController());
+// class StateController extends StateNotifier<ViewState> {
+//   StateController() : super(ViewState());
+//
+//   void increment() {
+//     state = ViewState()..count = state.count+1;
+//   }
+// }
+//endregion
 
 class ViewState{
-  int count = 0;
+  int count = 1;
 }
 
 class RView extends ConsumerStatefulWidget {
@@ -52,7 +74,6 @@ class _RViewState extends ConsumerState<RView> {
   @override
   Widget build(BuildContext context) {
     // ViewState state = ref.watch(stateControllerProvider);
-    print('build yi ci ');
     var count = ref.watch(stateControllerProvider).count;
     return Text(count.toString());
   }
@@ -68,17 +89,51 @@ class RController extends ConsumerStatefulWidget {
 }
 class _RControllerState extends ConsumerState<RController> {
 
+  int selfCount = 0;
+
   void _incrementCounter() {
-    // ref.read(stateControllerProvider.notifier).state++;
-    ref.read(stateControllerProvider.notifier).increment();
+    ref.read(stateControllerProvider).count++;
+    setState(() {
+      selfCount += 2;
+    });
+    // ref.read(stateControllerProvider.notifier).increment();
   }
 
   @override
   Widget build(BuildContext context) {
-    return FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      );
+    return
+    Column(
+      children: [
+        FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: '点击增加',
+          child: const Icon(Icons.add),
+        ),
+        Text('当前自身State值:$selfCount')
+      ],
+    );
+  }
+}
+
+
+class RStatelessView extends ConsumerWidget {
+  const RStatelessView({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    var count = ref.watch(stateControllerProvider).count + 100;
+    return Text('StateLess组件中使用值$count');
+  }
+}
+
+class RStatelessController extends ConsumerWidget {
+  const RStatelessController({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return TextButton(onPressed: (){
+      ref.read(stateControllerProvider).count++;
+    }, child:
+        const Text('点击我增加'),
+    );
   }
 }
